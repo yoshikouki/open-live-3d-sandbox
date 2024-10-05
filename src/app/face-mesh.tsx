3cfab982b926bcda5bbeb587e7278c7d753b974a
yoshikouki <yoshikouki@pepabo.com>
2024-10-05 12:47:04 +0900

    Show face mesh

--- src/app/face-mesh.tsx
+++ src/app/face-mesh.tsx
@@ -1,84 +1,87 @@
 "use client";
 
-import { FaceLandmarker, FilesetResolver } from "@mediapipe/tasks-vision";
-import { useEffect, useRef, useState } from "react";
+import { DrawingUtils, FaceLandmarker, FaceLandmarkerResult, FilesetResolver } from "@mediapipe/tasks-vision";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export const FaceMesh = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasCtxRef = useRef<CanvasRenderingContext2D | null>(null);
   const [faceLandmarker, setFaceLandmarker] = useState<FaceLandmarker | null>(
     null,
   );
-  const _lastVideoTimeRef = useRef(-1);
+  const lastVideoTimeRef = useRef(-1);
 
-  // const renderLoop = useCallback(() => {
-  //   const canvasCtx = canvasRef.current?.getContext("2d");
-  //   if (!videoRef.current || !canvasCtx) return;
-  //   let results: FaceLandmarkerResult | undefined;
-  //   const video = videoRef.current;
-  //   const drawingUtils = new DrawingUtils(canvasCtx);
+  const renderLoop = useCallback(() => {
+    if (!videoRef.current || !canvasCtxRef.current) return;
+    let results: FaceLandmarkerResult | undefined;
+    const drawingUtils = new DrawingUtils(canvasCtxRef.current);
+    const video = videoRef.current;
+    const canvas = canvasCtxRef.current.canvas;
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
 
-  //   if (video.currentTime !== lastVideoTimeRef.current) {
-  //     lastVideoTimeRef.current = video.currentTime;
-  //     try {
-  //       // results = faceLandmarker?.detectForVideo(video, performance.now());
-  //     } catch (error) {
-  //       console.error("顔検出中にエラーが発生しました:", error);
-  //       return;
-  //     }
-  //   }
-  //   if (!results) return;
-  //   for (const landmarks of results.faceLandmarks) {
-  //     drawingUtils.drawConnectors(
-  //       landmarks,
-  //       FaceLandmarker.FACE_LANDMARKS_TESSELATION,
-  //       { color: "#C0C0C070", lineWidth: 1 },
-  //     );
-  //     drawingUtils.drawConnectors(
-  //       landmarks,
-  //       FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
-  //       { color: "#FF3030" },
-  //     );
-  //     drawingUtils.drawConnectors(
-  //       landmarks,
-  //       FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
-  //       { color: "#FF3030" },
-  //     );
-  //     drawingUtils.drawConnectors(
-  //       landmarks,
-  //       FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
-  //       { color: "#30FF30" },
-  //     );
-  //     drawingUtils.drawConnectors(
-  //       landmarks,
-  //       FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
-  //       { color: "#30FF30" },
-  //     );
-  //     drawingUtils.drawConnectors(
-  //       landmarks,
-  //       FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
-  //       { color: "#E0E0E0" },
-  //     );
-  //     drawingUtils.drawConnectors(
-  //       landmarks,
-  //       FaceLandmarker.FACE_LANDMARKS_LIPS,
-  //       { color: "#E0E0E0" },
-  //     );
-  //     drawingUtils.drawConnectors(
-  //       landmarks,
-  //       FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
-  //       { color: "#FF3030" },
-  //     );
-  //     drawingUtils.drawConnectors(
-  //       landmarks,
-  //       FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
-  //       { color: "#30FF30" },
-  //     );
-  //   }
-  //   requestAnimationFrame(() => {
-  //     renderLoop();
-  //   });
-  // }, [faceLandmarker]);
+    lastVideoTimeRef.current = video.currentTime;
+    try {
+      results = faceLandmarker?.detectForVideo(video, performance.now());
+    } catch (error) {
+      console.error("顔検出中にエラーが発生しました:", error);
+      return;
+    }
+    if (!results) return;
+    for (const landmarks of results.faceLandmarks) {
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+        { color: "#C0C0C070", lineWidth: 1 },
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+        { color: "#FF3030" },
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+        { color: "#FF3030" },
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+        { color: "#30FF30" },
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+        { color: "#30FF30" },
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+        { color: "#E0E0E0" },
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LIPS,
+        { color: "#E0E0E0" },
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+        { color: "#FF3030" },
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+        { color: "#30FF30" },
+      );
+    }
+    requestAnimationFrame(() => {
+      renderLoop();
+    });
+    return () => {
+      faceLandmarker?.close();
+    };
+  }, [faceLandmarker]);
 
   useEffect(() => {
     (async () => {
@@ -107,24 +110,27 @@ export const FaceMesh = () => {
       .then((stream) => {
         if (!videoRef.current) return;
         videoRef.current.srcObject = stream;
-        // videoRef.current.addEventListener("loadeddata", renderLoop);
-        // renderLoop();
+        videoRef.current.addEventListener("loadeddata", renderLoop);
       });
-    return () => {
-      faceLandmarker?.close();
-    };
   }, [faceLandmarker]);
 
   return (
-    <div className="relative h-full w-full">
+    <div className="relative h-full w-full flex items-center justify-center">
       <video
         id="webcam"
         autoPlay
         playsInline
         ref={videoRef}
         className="h-full w-full"
+        style={{ width: "640px", height: "480px" }}
       />
-      <canvas ref={canvasRef} className="absolute inset-0 h-full w-full" />
+      <canvas
+        ref={(canvas) => {
+          if (!canvas) return;
+          canvasCtxRef.current = canvas?.getContext("2d");
+        }}
+        className="absolute"
+      />
     </div>
   );
 };
